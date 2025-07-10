99ebed3baa2661221ec978c53a75d95a0c5a771a
Rajendhiran E <rajendhiran.easu@liveramp.com>
2025-07-10 19:59:53 +0530

    verified on map updates

--- src/main/kotlin/basics/KTrails.kt
+++ src/main/kotlin/basics/KTrails.kt
@@ -9,7 +9,7 @@ fun main() {
 
     // Index Operator Overloading
 
-    val jTrails = JTrails()
+    /*val jTrails = JTrails()
 
     // getter
     println(jTrails["Tester"])
@@ -23,5 +23,15 @@ fun main() {
 
     // setter
     jTrails["Rajendhiran"] = 20
-    println("Setter Details: Name: ${jTrails.name} ; Age: ${jTrails.age}")
+    println("Setter Details: Name: ${jTrails.name} ; Age: ${jTrails.age}")*/
+
+    println(dataMap)
+    println(addData())
+    println(dataMap)
 }
+val dataMap = mutableMapOf("test" to "tester", "test1" to "tester1", "test2" to "tester2")
+
+fun addData(): Map<String, String> {
+    return dataMap + mapOf("test3" to "tester3", "test4" to "tester4", "test5" to "tester5")
+}
+
